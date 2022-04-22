741b2db1eb28ad34c612da7b5c4179d9b7b5b13e
Mendes007 <mendesartur99@gmail.com>
2022-04-22 10:02:30 -0300

    downgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
- import FormularioLogin from "./components/furmulario/formulario";
+
 interface IAppProps { }
 
 function App(props: IAppProps) {
     
     return (
         <div className="App">
-            
-             <FormularioLogin/>    
             
         </div>
     );
