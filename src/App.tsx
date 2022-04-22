f834a7ac39c70bdd27b4114fa9322e86f0aa60b3
Allyson1602 <aathyrson@yahoo.com.br>
2022-04-22 09:28:35 -0300

    Main limpa

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import Header from './components/header/header.component';
-import './App.css';
-import Footer from './components/footer/footer.component';
-import Comment, { IAuthor } from './components/comment/comment.component';
-import MinhaImagem from './assets/images/2.png';
-
 
 interface IAppProps { }
-
-let author: IAuthor = {
-    avatarUrl: MinhaImagem,
-    name: "Artur",
-}; 
 
 function App(props: IAppProps) {
     
     return (
         <div className="App">
-            <Header/>
-            <Comment author={author} text={"Bem vindo!"} date={new Date()} />
-            <Footer/>
+            
         </div>
     );
 };
