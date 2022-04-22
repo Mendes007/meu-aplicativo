173047238d155e487a1102231579d7ef47a4a93a
Mendes007 <mendesartur99@gmail.com>
2022-04-22 10:00:35 -0300

    versão formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-
+ import FormularioLogin from "./components/furmulario/formulario";
 interface IAppProps { }
 
 function App(props: IAppProps) {
     
     return (
         <div className="App">
+            
+             <FormularioLogin/>    
             
         </div>
     );
