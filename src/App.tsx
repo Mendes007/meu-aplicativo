0057fad715f2062cd26182d2bda11fcd290de3c4
Allyson1602 <aathyrson@yahoo.com.br>
2022-05-02 08:38:33 -0300

    atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TextField, Typography } from "@mui/material";
 
 interface IAppProps { }
 
@@ -5,7 +6,9 @@ function App(props: IAppProps) {
     
     return (
         <div className="App">
-            
+            <Typography variant={"h5"} className={'tipografia'}>Olá mundo</Typography>
+            <br/>
+            <TextField variant={"outlined"} label={"Meu campo"} size={"small"} color={"warning"}  disabled={true} />
         </div>
     );
 };
