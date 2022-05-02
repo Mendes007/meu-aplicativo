712bce010827523358b9ef5f257f70e5fbdc1fa4
Allyson1602 <aathyrson@yahoo.com.br>
2022-05-02 12:06:18 -0300

    Criação de cartão

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { TextField, Typography } from "@mui/material";
-
 interface IAppProps { }
 
 function App(props: IAppProps) {
     
     return (
         <div className="App">
-            <Typography variant={"h5"} className={'tipografia'}>Olá mundo</Typography>
-            <br/>
-            <TextField variant={"outlined"} label={"Meu campo"} size={"small"} color={"warning"}  disabled={true} />
+            
         </div>
     );
 };
